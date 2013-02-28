56beda23477ac8cda0cf033df714a596113ce165
Kaushik Gopal <code@kaush.co>
2013-02-28 09:26:25 -0800

    adding coverage data with a environment variable COVERAGE - SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,8 @@
 if ENV['COVERAGE']
   require 'simplecov'
   # SimpleCov.start 'rails'
-  SimpleCov.start do
-    add_filter '/spec/'
-    add_filter '/config/'
-    add_filter '/lib/'
-    add_filter '/vendor/'
-   
-    # add_group 'Controllers', 'app/controllers'
-    # add_group 'Models', 'app/models'
-    # add_group 'Helpers', 'app/helpers'
-    # add_group 'Mailers', 'app/mailers'
-    # add_group 'Views', 'app/views'
-    # add_group 'Requests', 'app/views'
-  end  
+  SimpleCov.start
 end
-
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
