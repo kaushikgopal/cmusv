560d532a95f684fdba15ab6883b21ac0343e1e27
Kaushik Gopal <code@kaush.co>
2013-06-06 14:46:33 -0700

    adding more tests to peer evaluation email

--- spec_no_rails/services/peer_evaluation_email_spec.rb
+++ spec_no_rails/services/peer_evaluation_email_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../app/services/peer_evaluation_email.rb'
+require_relative '../../app/mailers/generic_mailer.rb'
 
 describe PeerEvaluationEmail do
 
@@ -6,6 +7,12 @@ describe PeerEvaluationEmail do
 	  it "should respond to send_email" do
 	  	PeerEvaluationEmail.should respond_to(:send_email)
 	  end
+
+	  xit "should deliver email with options" do
+	    options =
+	    GenericMailer.should_receieve(:email).with(options)
+	  end
+
 	end
 
 	context "send peer evaluation email" do
